6960cb6556bfc9ec64bd70a5215d97497476964a
anaisuzan <anais12393@gmail.com>
2025-08-07 10:04:25 +0200

    boutons OK

--- back/server.js
+++ back/server.js
@@ -5,21 +5,19 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors("*"))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-let move ="LEFT";
-let action ="COLLECT";
 
-app.get('/action', (req, res) => {
-    res.json({move, action})
-  })
+// let move ="LEFT";
+// let action ="COLLECT";
+
+// app.get('/action', (req, res) => {
+//     res.json({move, action})
+//   })
 
   app.post('/action', (req, res) => {
     console.log(req.body)
     // Récupération des données du body
-    move = req.body?.move;
-    action = req.body?.action;
+    const move = req.body?.move;
+    const action = req.body?.action;
     // Réponse
     res.json({ move, action });
   });
