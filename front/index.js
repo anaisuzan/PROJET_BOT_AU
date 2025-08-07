175aa3bcb0a0fecb937736aec673c795ec779945
anaisuzan <anais12393@gmail.com>
2025-08-07 10:09:57 +0200

    boutons OK

--- front/index.js
+++ front/index.js
@@ -1,19 +1,32 @@
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-var raw = JSON.stringify({
-  "move": "UP",
-  "action": "NONE"
+document.querySelectorAll("button").forEach(button => {
+  button.addEventListener("click", () => {
+    let move = "NONE";
+    let action = "NONE";  
+
+    if (button.dataset.move) {
+      move = button.dataset.move;
+    }
+    if (button.dataset.action) {
+      action = button.dataset.action;
+    }
+
+    var raw = JSON.stringify({
+      move, action
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("http://localhost:3000/action", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.error("error:", error));
+  });
 });
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:3000/action", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
